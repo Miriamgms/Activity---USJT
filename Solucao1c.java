30925d635dd1e37b3737316ff15c152f8ba04a88
Miriam <miriamgama456@gmail.com>
2024-09-12 20:22:06 -0300

    Add files via upload

---
+++ Solucao1c.java
@@ -0,0 +1,13 @@
+import javax.swing.JOptionPane;
+    public class Solucao1c
+  {
+     public static void main(String[] args) 
+     {
+      //lendo números
+     String dp1 = JOptionPane.showInputDialog("Digite um dia do ano:");
+     String dp2 = JOptionPane.showInputDialog("Digite um mês do ano:");
+     String dp3 = JOptionPane.showInputDialog("Digite o ano:");
+     //transforme em data de barras
+   JOptionPane.showMessageDialog(null,"Data = " + dp1 + "/" + dp2 + "/" + dp3);
+      }
+     }
