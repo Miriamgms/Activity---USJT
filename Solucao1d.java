3d403b8517fbe2869092c074942c8c87c90b4f9a
Miriam <miriamgama456@gmail.com>
2024-09-12 20:25:42 -0300

    Add files via upload

---
+++ Solucao1d.java
@@ -0,0 +1,11 @@
+import javax.swing.JOptionPane;
+    public class Solucao1d
+  {
+     public static void main(String[] args) 
+     {
+      //Apresentando valor
+     String speso = JOptionPane.showInputDialog("Digite seu peso em formato real:");
+     //transforme em kg
+   JOptionPane.showMessageDialog(null,"Resultado = " + speso + "kg");
+      }
+     }
