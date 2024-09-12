2dec0bc9349ea1affa38dcf8036fbe218e3b4615
Miriam <miriamgama456@gmail.com>
2024-09-12 20:14:40 -0300

    Add files via upload

---
+++ Solucao1b.java
@@ -0,0 +1,14 @@
+import javax.swing.JOptionPane;
+  public class Solucao1b
+ {
+   public static void main(String[] args) 
+     {
+     //colocando número inteiro
+     String sInteiro = JOptionPane.showInputDialog
+       ("Digite um número inteiro (ex: 123):");
+        // Guardar e exibir valor
+         JOptionPane.showMessageDialog(null,"Numero = " + sInteiro);
+         }
+        } 
+
+
