35ae6320ccc1bf7be43ab2c0555cf4e81c438a81
Miriam <miriamgama456@gmail.com>
2024-09-12 20:01:15 -0300

    Add files via upload

---
+++ Solucao1a.java
@@ -0,0 +1,13 @@
+import javax.swing.JOptionPane;
+    public class Solucao1a
+  {
+     public static void main(String[] args) 
+     {
+      //lendo três palavras
+     String p1 = JOptionPane.showInputDialog("Digite a primeira palavra:");
+     String p2 = JOptionPane.showInputDialog("Digite a segunda palavra:");
+     String p3 = JOptionPane.showInputDialog("Digite a terceira palavra:");
+     //mudar ordem das palavras
+   JOptionPane.showMessageDialog(null,"Palavras = " + p3 + " " + p2 + " " + p1);
+      }
+     }
