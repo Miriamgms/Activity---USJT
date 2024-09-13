e132be132ab55be90a3ca9c240c7b1c6ee57e2fb
Miriam <miriamgama456@gmail.com>
2024-09-13 11:48:58 -0300

    Solucao1G.java

---
+++ Solucao1G.java
@@ -0,0 +1,17 @@
+//exercicio aula 01 - 1G
+import javax.swing.JOptionPane;
+  public class Solucao1G
+{
+  public static void main(String[] args)
+  {
+      //apresentar idade - P-D-U
+       String sIdade = JOptionPane.showInputDialog("Digite sua idade:");
+        int idade = integer.parseInt (sIdade);
+            //inteiro calcular
+        int diasVividos = idade * 365;
+          //exibição
+   JOptionPane.showMessageDialog(null, "você viveu aproximadamente " + diasVividos +  “dias.”);
+   }
+}
+
+//Anotações: integer.parseInt =  converter uma string em um número inteiro Int = número inteiro
