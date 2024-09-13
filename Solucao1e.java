88b017bb0983e8122318ea688c0918171c84828f
Miriam <miriamgama456@gmail.com>
2024-09-13 09:41:16 -0300

    Solucao1e.java

---
+++ Solucao1e.java
@@ -0,0 +1,12 @@
+//exercicio aula 01 - 5A
+import javax.swing.JOptionPane;
+  public class Solucaoe
+{
+  public static void main(String[] args)
+  {
+      //apresentar
+ char[] caracteres = {'C', 'o', 'm', 'p', 'u', 't', 'a', 'd', 'o', 'r'};
+// imprimindo
+ System.out.println("A palavra formada é: " + caracteres);
+    }
+}
