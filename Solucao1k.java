038f8d23e99658f8ebcf9919e54858a0bfc8d66c
Miriam <miriamgama456@gmail.com>
2024-09-13 14:47:17 -0300

    Create Solucao1k.java

---
+++ Solucao1k.java
@@ -0,0 +1,23 @@
+//exercicio Aula 01 - 1K
+import javax.swing.JOptionPane;
+  public class Solucao1k
+{
+  public static void main(String[] args)
+  {
+      //apresentação e leitura de dados
+       String sValorDoInicio = JOptionPane.showInputDialog(
+         "Digite o valor inicial da sua dívida:");
+        String sJuros = JOptionPane.showInputDialog(
+          "Digite a taxa de juros (%):");
+        String sMeses = JOptionPane.showInputDialog(
+        “Digite o número de meses:”);
+            //convertendo para real
+      double valorDoInicio = Double.parseDouble(sValorDoInicio);
+      double juros = Double.parseDouble(sJuros);
+      Int meses = integer.parseInt(sMeses);
+          //calculando
+       double valorFinal = valorDoInicio * Mathpow(1 + juros / 100, meses);
+       //apresentando o resultado
+   JOptionPane.showMessageDialog(null, "O valor final da divida é = " + valorFinal);
+   }
+}
