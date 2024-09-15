35724a0d92d1d2bc3f509052ba3d3914727465bf
Miriam <miriamgama456@gmail.com>
2024-09-15 12:12:37 -0300

    Atualizar o Solucao1g.java

--- Solucao1g.java
+++ Solucao1g.java
@@ -7,8 +7,10 @@ import javax.swing.JOptionPane;
       //apresentar idade - P-D-U
        String sIdade = JOptionPane.showInputDialog("Digite sua idade:");
         int idade = integer.parseInt (sIdade);
+JOptionPane.showInputDialog("Digite quantos dias tem no ano:");
+        int dias = integer.parseInt (sDias);
             //inteiro calcular
-        int diasVividos = idade * 365;
+        int diasVividos = idade * dias;
           //exibição
    JOptionPane.showMessageDialog(null, "você viveu aproximadamente " + diasVividos +  “dias.”);
    }
