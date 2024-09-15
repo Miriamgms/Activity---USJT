3465d5cfe895174da8bd7522350a07250bf2c4b9
Miriam <miriamgama456@gmail.com>
2024-09-15 12:50:37 -0300

    Atualizar o Solucao1i.java

--- Solucao1i.java
+++ Solucao1i.java
@@ -1,18 +1,18 @@
 //exercicio aula 01 - 1i
 import javax.swing.JOptionPane;
-  public class Solucao1i
+  public class Solucao1iMiriam
 {
   public static void main(String[] args)
   {
       //leitura de valor
        String sValor = JOptionPane.showInputDialog("Digite um número inteiro:");
-        double valor = Double.parseDouble(sValor);
-            //elevando ao quadrado
-        Double resultado = Math.pow(valor, 2);
-          //exibir
-   JOptionPane.showMessageDialog(null, “O numero ao quadrado é:” + resultado);
+       int valor = Integer.parseInt (sValor);
+       
+     //System.out.println ( "O valor digitado ao quadrado = " + Math.pow(valor,2));    
+     JOptionPane.showMessageDialog (null, "O valor digitado ao quadrado = " + Math.pow(valor,2));
    }
 }
+  
 
 //*Anotações: Math.pow = A linguagem Java está equipada com uma biblioteca abrangente para funções e operações matemáticas. Ela é chamada de classe "Math" e reside no pacote java.lang 
 //A biblioteca Math inclui métodos para operações numéricas fundamentais, trigonometria, encontrar min-max, gerar números aleatórios e operações logarítmicas. 
