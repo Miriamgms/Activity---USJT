ec65a0e98a170933c38437618d46c6c024f01255
Miriam <miriamgama456@gmail.com>
2024-09-17 08:15:53 -0300

    Update Solucao1e.java

--- Solucao1e.java
+++ Solucao1e.java
@@ -1,18 +1,12 @@
-//exercicio aula 01 - 5
+/*exercicio 5 Aula: 01
+Estrutura básica de um programa Java, Entrada e Saída, Variáveis, Atribuição,
+Conversão de Strings*/
   public class Solucao1e
 public static void main(String[] args)
   {
       //apresentar
- char c = 'c';
- char o = 'o';
- char m = 'm';
- char p = 'p';
- char u = 'u';
- char t = 't';
- char a = 'a';
- char d = 'd';
- char oo = 'o';
- char r = 'r';
+ char c = 'c'; char o = 'o'; char m = 'm'; char p = 'p'; char u = 'u'; char t = 't'; char a = 'a'; char d = 'd';
+ char oo = 'o';char r = 'r';
  // imprimindo
  System.out.println(c);
  System.out.println(o);
