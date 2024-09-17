e60f3289bda5ebefb7fb72bdb2c8a0db05eb8e7b
Miriam <miriamgama456@gmail.com>
2024-09-17 07:49:43 -0300

    Update Solucao1g.java

--- Solucao1g.java
+++ Solucao1g.java
@@ -1,4 +1,4 @@
-//exercicio aula 01 - Operadores Aritméticos, Expressões Aritméticas e Strings'7'
+//exercicio aula 01 - Operadores Aritméticos, Expressões Aritméticas e Strings'1'
 import javax.swing.JOptionPane;
   public class Solucao1g
 {
