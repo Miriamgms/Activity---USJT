15042dfba2ec52188fcea547bcc9fcf90c8ff1b0
Miriam <miriamgama456@gmail.com>
2024-09-16 21:10:27 -0300

    Update Solucao1e.java

--- Solucao1e.java
+++ Solucao1e.java
@@ -1,11 +1,28 @@
 //exercicio aula 01 - 5
   public class Solucao1e
-{
-  public static void main(String[] args)
+public static void main(String[] args)
   {
       //apresentar
- char[] caracteres = {'C', 'o', 'm', 'p', 'u', 't', 'a', 'd', 'o', 'r'};
-// imprimindo
- System.out.println("A palavra formada é: " + caracteres);
+ char c = 'c';
+ char o = 'o';
+ char m = 'm';
+ char p = 'p';
+ char u = 'u';
+ char t = 't';
+ char a = 'a';
+ char d = 'd';
+ char oo = 'o';
+ char r = 'r';
+ // imprimindo
+ System.out.println(c);
+ System.out.println(o);
+ System.out.println(m);
+ System.out.println(p);
+ System.out.println(u);
+ System.out.println(t);
+ System.out.println(a);
+ System.out.println(d);
+ System.out.println(o);
+ System.out.println(r);
     }
 }
