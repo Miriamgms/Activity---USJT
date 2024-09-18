72b63630c8364df2c03a4adc21d56a88c49cc672
Miriam <miriamgama456@gmail.com>
2024-09-17 21:44:19 -0300

    Update Solucao1l.java

--- Solucao1l.java
+++ Solucao1l.java
@@ -4,19 +4,19 @@ import javax.swing.JOptionPane;
 {
      public static void main(String[] args) 
      {
-              // Ler o número inteiro de 3 casas decimais usando JOptionPane
+              // Ler o número inteiro de 3 casas decimais 
      String input = JOptionPane.showInputDialog
 ("Digite um número inteiro de 3 casas decimais (ex: 123):");
             // Converter a entrada para um inteiro
     int numero = Integer.parseInt(input);
-          // Validar se o número tem exatamente 3 dígitos
+          // confirmar casas decimais
                 if (numero < 100 || numero > 999) {
        JOptionPane.showMessageDialog(null, "Por favor, digite um número inteiro de 3 casas decimais.");
 return;
 }
-          // Extrair o algarismo da casa das dezenas
+          //pegar dezena
                 int dezenas = (numero / 10) % 10;
-           // Exibir o algarismo da casa das dezenas
+           // Apresentar
        JOptionPane.showMessageDialog(null, "O algarismo da casa das dezenas é " + dezenas);
       }
 }
