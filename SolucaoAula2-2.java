4f44f0dcdda311c84e2ae529dde6a9b978611d98
Miriam <miriamgama456@gmail.com>
2024-09-20 23:43:31 -0300

    Create SolucaoAula2-2.java

---
+++ SolucaoAula2-2.java
@@ -0,0 +1,28 @@
+/* Aula: 02 Desvio Condicional if, if–else, if–else if–else; Operadores Relacionais e Operadores Lógicos */
+import javax.swing.JOptionPane;
+public class Solucao2
+{ 
+    public static void main(String[] args) 
+  {
+    //lendo dois números
+  String sValor1 = JOptionPane.showInputDialog("Digite o primeiro número:");
+  String sValor2 = JOptionPane.showInputDialog("Digite o segundo número:");
+
+        // Convertendo para int
+        int numero1 = Integer.parseInt(sValor1);
+        int numero2 = Integer.parseInt(sValor2);
+
+        // adição
+        int soma = numero1 + numero2;
+
+        // Utilizar if e else para verificar se a soma é menor ou igual a 20
+        if (soma <= 20) 
+        {
+             // Subtrai 5 da soma 
+                  soma -= 5; 
+        }
+
+        // Resultado apresentado subtraindo-se 5.
+        JOptionPane.showMessageDialog(null, "O resultado é: " + soma);
+    }
+}
