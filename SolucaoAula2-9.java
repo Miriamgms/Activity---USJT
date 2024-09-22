401d09f2acbf1044bc56a58f52f3095898540f10
Miriam <miriamgama456@gmail.com>
2024-09-22 14:47:22 -0300

    Update SolucaoAula2-9.java

--- SolucaoAula2-9.java
+++ SolucaoAula2-9.java
@@ -5,8 +5,10 @@ public class Solucao9
  {
     public static void main(String[] args) 
     {
-        int numero = Integer.parseInt(JOptionPane.showInputDialog("Digite um número:"));
-
+        String sNumero = JOptionPane.showInputDialog("Digite um número:");
+     //convertendo para int
+        int numero = Integer.parseInt(sNumero);
+     // utilizando if e else para válidar condição
         if (numero % 3 == 0) 
         {
             JOptionPane.showMessageDialog(null, numero + " é múltiplo de 3.");
@@ -18,5 +20,5 @@ public class Solucao9
     }
 }
 
- /* O operador % é o operador de módulo em Java. Ele retorna o resto da divisão de um número pelo outro. No código acima, `numero % 3´ verifica se o resto da divisão de numero por 3 é igual a 0.
+ /* O operador % é o operador de módulo em Java. Ele retorna o resto da divisão de um número pelo outro. No código acima, numero % 3 verifica se o resto da divisão de numero por 3 é igual a 0.
   Se for, isso significa que numero é múltiplo de 3*/
