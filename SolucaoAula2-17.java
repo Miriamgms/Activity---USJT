6c0a49ae3ed72b1dcfab063cfb9f78a6b383c78f
Miriam <miriamgama456@gmail.com>
2024-09-22 20:45:22 -0300

    Create SolucaoAula2-17.java

---
+++ SolucaoAula2-17.java
@@ -0,0 +1,23 @@
+/* Aula: 02 Desvio Condicional if, if–else, if–else if–else; Operadores Relacionais e Operadores Lógicos */
+import javax.swing.JOptionPane;
+
+public class Solucao17 
+{
+    public static void main(String[] args) 
+    {
+        String sNum1 = JOptionPane.showInputDialog("Digite um número:");
+        String sNum2 = JOptionPane.showInputDialog("Digite um número:");
+        
+        int num1 = Integer.parseInt(sNum1);
+        int num2 = Integer.parseInt(sNum2);
+       //convertendo números em int e aplicando no if para válidar condição
+        if (num1 < num2) 
+        {
+            JOptionPane.showMessageDialog(null, "O menor número é: " + num1);
+        } 
+        if (num1 > num2)
+         {
+            JOptionPane.showMessageDialog(null, "O menor número é: " + num2);
+        }
+    }
+}
