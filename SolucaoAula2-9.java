569d93ea41a0232b760fe55e4dd0d7e54cb94eac
Miriam <miriamgama456@gmail.com>
2024-09-22 14:43:09 -0300

    Create SolucaoAula2-9.java

---
+++ SolucaoAula2-9.java
@@ -0,0 +1,22 @@
+/* Aula: 02 Desvio Condicional if, if–else, if–else if–else; Operadores Relacionais e Operadores Lógicos */
+import javax.swing.JOptionPane;
+
+public class Solucao9
+ {
+    public static void main(String[] args) 
+    {
+        int numero = Integer.parseInt(JOptionPane.showInputDialog("Digite um número:"));
+
+        if (numero % 3 == 0) 
+        {
+            JOptionPane.showMessageDialog(null, numero + " é múltiplo de 3.");
+        } 
+        else 
+        {
+            JOptionPane.showMessageDialog(null, numero + " não é múltiplo de 3.");
+        }
+    }
+}
+
+ /* O operador % é o operador de módulo em Java. Ele retorna o resto da divisão de um número pelo outro. No código acima, `numero % 3´ verifica se o resto da divisão de numero por 3 é igual a 0.
+  Se for, isso significa que numero é múltiplo de 3*/
