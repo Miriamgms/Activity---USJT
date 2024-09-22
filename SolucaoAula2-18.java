c27a921476f440abb0a04122f65a9e495d22d5db
Miriam <miriamgama456@gmail.com>
2024-09-22 20:59:00 -0300

    Create SolucaoAula2-18.java

---
+++ SolucaoAula2-18.java
@@ -0,0 +1,26 @@
+/* Aula: 02 Desvio Condicional if, if–else, if–else if–else; Operadores Relacionais e Operadores Lógicos */
+import javax.swing.JOptionPane;
+
+public class Solucao18 
+{
+    public static void main(String[] args) 
+    {
+        // Ler os dois números
+        String sNum1 = JOptionPane.showInputDialog("Digite o primeiro número:");
+        String sNum2 = JOptionPane.showInputDialog("Digite o segundo número:");
+
+        // Converter para int
+        int primeiroNumero = Integer.parseInt(sNum1);
+        int segundoNumero = Integer.parseInt(sNum2);
+
+        // Verificar e imprimir em ordem crescente
+        if (primeiroNumero < segundoNumero) 
+        {
+            JOptionPane.showMessageDialog(null, "Ordem crescente: " + primeiroNumero + ", " + segundoNumero);
+        } 
+        else 
+        {
+            JOptionPane.showMessageDialog(null, "Ordem crescente: " + segundoNumero + ", " + primeiroNumero);
+        }
+    }
+}
