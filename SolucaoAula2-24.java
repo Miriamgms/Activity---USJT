33e4f8f66992b19fc3d6a962cfc62bf6104c49f0
Miriam <miriamgama456@gmail.com>
2024-09-22 22:07:57 -0300

    Create SolucaoAula2-24.java

---
+++ SolucaoAula2-24.java
@@ -0,0 +1,34 @@
+import javax.swing.JOptionPane;
+
+public class Solucao24 
+{
+    public static void main(String[] args) 
+    {
+        int maior = Integer.MIN_VALUE;
+        int menor = Integer.MAX_VALUE;
+
+        for (int i = 1; i <= 5; i++) 
+        {
+            String snumeros = JOptionPane.showInputDialog("Digite o " + i + "º número:");
+            int numero = Integer.parseInt(snumeros);
+
+            // Verificar se é o maior
+            if (numero > maior) 
+            {
+                maior = numero;
+            }
+            // Verificar se é o menor
+            if (numero < menor) 
+            {
+                menor = numero;
+            }
+        }
+
+        JOptionPane.showMessageDialog(null, "O maior número é: " + maior + "\nO menor número é: " + menor);
+    }
+}
+
+
+/*Sobre o FOR: O loop permite que o programa solicite ao usuário que insira cinco números, um por vez. 
+Após cada entrada, ele processa o número para verificar se é o maior ou o menor até aquele momento. 
+Assim, o for é fundamental para repetir a solicitação de entrada e as comparações.*/
