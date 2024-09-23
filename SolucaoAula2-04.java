6de66a73ae626040935264623d5b9b062fe10ed4
Miriam <miriamgama456@gmail.com>
2024-09-22 21:01:55 -0300

    Update SolucaoAula2-04.java

--- SolucaoAula2-04.java
+++ SolucaoAula2-04.java
@@ -1,68 +1,28 @@
+/* Aula: 02 Desvio Condicional if, if–else, if–else if–else; Operadores Relacionais e Operadores Lógicos */
 import javax.swing.JOptionPane;
 
 public class Solucao4 
-{ 
+{
     public static void main(String[] args) 
     {
-        // Lendo três números
-        String sValor1 = JOptionPane.showInputDialog("Digite o primeiro número:");
-        String sValor2 = JOptionPane.showInputDialog("Digite o segundo número:");
-        String sValor3 = JOptionPane.showInputDialog("Digite o terceiro número:");
-        
-        // Convertendo para int
-        int num1 = Integer.parseInt(sValor1);
-        int num2 = Integer.parseInt(sValor2);
-        int num3 = Integer.parseInt(sValor3);
-        
-        // Armazenar os números em ordem crescente
-        int menor, meio, maior;
-        
-        // Utilizando if e else para determinar a ordem crescente
-        if (num1 <= num2 && num1 <= num3) 
+        // Ler os dois números
+        String sNum1 = JOptionPane.showInputDialog("Digite o primeiro número:");
+        String sNum2 = JOptionPane.showInputDialog("Digite o segundo número:");
+        String sNum3 = JOptionPane.showInputDialog("Digite o terceiro número:");
+
+        // Converter para int
+        int primeiroNumero = Integer.parseInt(sNum1);
+        int segundoNumero = Integer.parseInt(sNum2);
+        int terceiroNumero = Integer.parseInt(sNum3);
+
+        // Verificar e imprimir em ordem crescente
+        if (primeiroNumero < segundoNumero && segundoNumero < terceiroNumero) 
         {
-            menor = num1;
-            if (num2 <= num3) 
-            {
-                meio = num2;
-                maior = num3;
-            } 
-            else 
-            {
-                meio = num3;
-                maior = num2;
-            }
-        } 
-        else if (num2 <= num1 && num2 <= num3) 
-        {
-            menor = num2;
-            if (num1 <= num3) 
-            {
-                meio = num1;
-                maior = num3;
-            }
-             else
-              {
-                meio = num3;
-                maior = num1;
-            }
+            JOptionPane.showMessageDialog(null, "Ordem crescente: " + primeiroNumero + ", " + segundoNumero + ", " + terceiroNumero);
         } 
         else 
         {
-            menor = num3;
-            if (num1 <= num2) 
-            {
-                meio = num1;
-                maior = num2;
-            } 
-            else
-             {
-                meio = num2;
-                maior = num1;
-            }
+            JOptionPane.showMessageDialog(null, "Ordem crescente: " + terceiroNumero + ", "  + segundoNumero + ", " + primeiroNumero);
         }
-        
-        // Apresentar os números em ordem crescente
-        String resultado = menor + ", " + meio + ", " + maior;
-        JOptionPane.showMessageDialog(null, "Os números em ordem crescente são: " + resultado);
     }
 }
