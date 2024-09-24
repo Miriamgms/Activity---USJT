ad9370b82b79faa6376ba361b387cb1914261cb4
Miriam <miriamgama456@gmail.com>
2024-09-23 22:29:19 -0300

    Update and rename SolucaoAula2-08.java to SolucaoAula2_08.java

--- SolucaoAula2_08.java
+++ SolucaoAula2_08.java
@@ -1,7 +1,7 @@
 /* Aula: 02 Desvio Condicional if, if–else, if–else if–else; Operadores Relacionais e Operadores Lógicos */
 import javax.swing.JOptionPane;
 
-public class Solucao8 
+public class SolucaoAula2_08
 {
     public static void main(String[] args) 
     {
