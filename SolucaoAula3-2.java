ad6bc3261c000db7e4622b3acf61597dc252f298
Miriam <miriamgama456@gmail.com>
2024-09-23 21:16:56 -0300

    Create SolucaoAula3-2.java

---
+++ SolucaoAula3-2.java
@@ -0,0 +1,16 @@
+/*Aula: 03
+Estruturas de Repetição: laços while, do-while e for*/
+import javax.swing.JOptionPane;
+
+public class Solucao03 {
+
+    public static void main(String[] args) {
+        int i = 1;
+        while (i <= 100){
+        if( i % 2 == 0){
+            JOptionPane.showMessageDialog(null, i  );
+        }
+        i++;
+        }
+    }
+}
