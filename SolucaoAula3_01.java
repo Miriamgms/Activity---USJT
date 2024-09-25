a88b1d97739cd7eafce6586b1c7a5bd8ec2d5328
Miriam <miriamgama456@gmail.com>
2024-09-24 22:29:16 -0300

    Update SolucaoAula3_01.java

--- SolucaoAula3_01.java
+++ SolucaoAula3_01.java
@@ -2,29 +2,34 @@
 Estruturas de Repetição: laços while, do-while e for
 import javax.swing.JOptionPane;*/
 
+import javax.swing.JOptionPane;
+
 public class SolucaoAula3_01
 {
     public static void main(String[] args)
-     {
-        int i = 1;
-
-        while (i <= 100) 
         {
-            StringBuilder numeros = new StringBuilder();
+        StringBuilder numeros = new StringBuilder();
 
-            // Adiciona 50 números ao StringBuilder
-            int count = 0;
-            while (count < 50 && i <= 100) {
-                numeros.append(i).append("\n");
-                i++;
-                count++;
+        // Loop para os números de 1 a 100
+        for (int i = 1; i <= 100; i++) {
+            numeros.append(i); // Adiciona o número
+
+            // Adiciona um hífen após o número, exceto após o último número
+            if (i < 100) {
+                numeros.append(" - ");
             }
 
-            // Exibe os números acumulados
-            JOptionPane.showMessageDialog(null, numeros.toString(), "Números", JOptionPane.INFORMATION_MESSAGE);
+            // A cada 20 números, adiciona uma nova linha
+            if (i % 20 == 0) {
+                numeros.append("\n");
+            }
         }
+
+        // Exibe os números acumulados
+        JOptionPane.showMessageDialog(null, numeros.toString(), "Números (1 a 100)", JOptionPane.INFORMATION_MESSAGE);
     }
 }
+
 
 /*StringBuilder é uma classe em Java que permite criar e manipular sequências de caracteres de maneira eficiente. 
 Ao contrário da classe String, que é imutável (ou seja, não pode ser alterada após ser criada), o StringBuilder pode ser modificado. 
