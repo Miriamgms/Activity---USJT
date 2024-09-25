1af6aa91bef24334b412b713ca79e54c65b112f0
Miriam <miriamgama456@gmail.com>
2024-09-24 22:15:36 -0300

    SolucaoAula3_07.java

---
+++ SolucaoAula3_07.java
@@ -0,0 +1,15 @@
+import javax.swing.JOptionPane;
+
+public class SolucaoAula3_07 
+{
+    public static void main(String[] args) 
+    {
+        StringBuilder multiplosde5 = new StringBuilder();
+        for ( int i = 1; i <= 500; i++){
+           if (i % 5 == 0){
+            multiplosde5.append(i).append("\n");
+           }
+        }
+        JOptionPane.showMessageDialog(null,  multiplosde5.toString(), "Números Múltiplos de 5 de 1 à 500:", JOptionPane.INFORMATION_MESSAGE);
+    }
+}
