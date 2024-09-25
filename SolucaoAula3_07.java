e2c296ec2d8ad88815fab63e706d10771fca88f9
Miriam <miriamgama456@gmail.com>
2024-09-25 13:43:27 -0300

    Update SolucaoAula3_07.java

--- SolucaoAula3_07.java
+++ SolucaoAula3_07.java
@@ -15,3 +15,18 @@ public class SolucaoAula3_07
         JOptionPane.showMessageDialog(null,  multiplosde5.toString(), "Números Múltiplos de 5 de 1 à 500:", JOptionPane.INFORMATION_MESSAGE);
     }
 }
+
+
+
+/*A classe StringBuilder em Java é usada para criar e manipular strings de forma mais eficiente do que usando a classe String comum. 
+Aqui estão algumas de suas principais características e vantagens:
+
+Mutabilidade: Diferente da classe String, que é imutável (ou seja, uma vez criada, não pode ser alterada),
+StringBuilder permite modificar o conteúdo da string sem criar novos objetos.
+
+Performance: Quando você precisa fazer muitas operações de concatenação ou modificação de strings, 
+StringBuilder é mais eficiente em termos de desempenho. Isso ocorre porque ele mantém um buffer interno 
+que pode ser alterado, evitando a criação de múltiplos objetos.
+
+Métodos úteis: StringBuilder possui diversos métodos que facilitam a manipulação de strings, como append(), 
+insert(), delete(), reverse(), entre outros.*/
