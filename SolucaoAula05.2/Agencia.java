f0b6bf1f875e566d558aeb4780d948dcccad053e
Miriam <miriamgama456@gmail.com>
2024-10-17 07:47:36 -0300

    Create Agencia.java

---
+++ SolucaoAula05.2/Agencia.java
@@ -0,0 +1,57 @@
+
+import javax.swing.JOptionPane;
+
+public class Agencia {
+    // Atributos
+    private String nome;
+    private int numero;
+    private int digito;
+
+    // Método Construtor
+    public Agencia(String nome, int numero, int digito) {
+        this.nome = nome;
+        setNumero(numero); 
+        setDigito(digito);
+    }
+
+    // Métodos de acesso
+    public String getNome() {
+        return nome;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public int getDigito() {
+        return digito;
+    }
+
+    // Métodos modificadores
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setNumero(int numero) {
+        if (numero >= 100 && numero <= 999) { 
+            this.numero = numero;
+        } 
+        else {
+            JOptionPane.showMessageDialog(null, "Número da agência deve ter exatamente 3 dígitos.", "Erro", JOptionPane.ERROR_MESSAGE);
+        }
+    }
+
+    public void setDigito(int digito) {
+        if (digito >= 0 && digito <= 9) { 
+            this.digito = digito;
+        } 
+        else {
+            JOptionPane.showMessageDialog(null, "Dígito da agência deve ser entre 0 e 9.", "Erro", JOptionPane.ERROR_MESSAGE);
+        }
+    }
+
+  
+    public String toString() {
+        return nome + " (" + numero + "-" + digito + ")";
+    }
+}
