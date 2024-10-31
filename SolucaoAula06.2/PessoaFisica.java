ce3adc343b924ad75617f42c59529e55f9ef22c2
Miriam <miriamgama456@gmail.com>
2024-10-30 22:05:54 -0300

    Update PessoaFisica.java

--- SolucaoAula06.2/PessoaFisica.java
+++ SolucaoAula06.2/PessoaFisica.java
@@ -1,1 +1,54 @@
+public class PessoaFisica {
+    // Atributos
+    private String nome;
+    private String sobrenome;
+    private String cpf;
+
+    // Construtor sem parâmetros
+    public PessoaFisica() {
+        setNome("Nome Padrão");
+        setSobrenome("Sobrenome Padrão");
+        setCpf("000.000.000-00");
+    }
+
+    // Construtor com parâmetros
+    public PessoaFisica(String nome, String sobrenome, String cpf) {
+        setNome(nome);
+        setSobrenome(sobrenome);
+        setCpf(cpf);
+    }
+
+    // Métodos de acesso
+    public String getNome() {
+        return nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    // Métodos modificadores
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    // Método que retorna os dados em formato String
+    public String dados() {
+        return "Nome: " + nome + "\n" +
+               "Sobrenome: " + sobrenome + "\n" +
+               "CPF: " + cpf;
+    }
+}
 
