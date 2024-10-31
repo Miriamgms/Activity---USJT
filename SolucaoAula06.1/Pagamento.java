ce7595e119cf8013c35bcbe39e028249bfa28011
Miriam <miriamgama456@gmail.com>
2024-10-30 21:33:20 -0300

    Update Pagamento.java

--- SolucaoAula06.1/Pagamento.java
+++ SolucaoAula06.1/Pagamento.java
@@ -1,1 +1,44 @@
-
+public class Pagamento{
+    //atributos
+    private String nomeDoPagador;
+    private String cpf;
+    private double valoraserpago;
+    
+    
+    // Construtor
+    public Pagamento(String n, String c, double v)
+    {
+    nomeDoPagador = n;
+    cpf = c;
+    valoraserpago = v;
+    }
+    
+    
+    // Métodos de acesso
+    public String getNomeDoPagador(){
+    return nomeDoPagador;
+    }
+    
+    public String getCpf(){
+    return cpf;
+    }
+    
+    public double getValoraserpago(){
+    return valoraserpago;
+    }
+    
+    
+    
+    // Métodos modificadores
+    public void setNomeDoPagador(String n){
+    nomeDoPagador = n;
+    }
+    
+    public void setCpf(String c){
+    cpf = c;
+    }
+    
+    public void setValoraserpago(double v){
+    valoraserpago = v;
+    }
+    }
