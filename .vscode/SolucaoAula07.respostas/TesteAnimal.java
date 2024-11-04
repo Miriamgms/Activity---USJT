f502c010defe98c4d82b77a04b19d6d7c65543f2
Miriam <miriamgama456@gmail.com>
2024-11-04 11:15:07 -0300

    Update TesteAnimal.java

--- .vscode/SolucaoAula07.respostas/TesteAnimal.java
+++ .vscode/SolucaoAula07.respostas/TesteAnimal.java
@@ -1,1 +1,17 @@
+//Solução Aula 07 - Exercício 1
 
+/*public class Animal
+{
+public void mover()
+ {
+System.out.println("O animal está se movendo.");
+ }
+}*/
+
+//Classe teste
+public class TesteAnimal{
+public static void main(String[] args){
+Animal animal = new Animal();
+animal.mover();
+ }
+}
