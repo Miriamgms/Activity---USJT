daa7f2b395ab712bd264c3cf680554d289a778c9
Miriam <miriamgama456@gmail.com>
2024-11-04 11:23:59 -0300

    Create TesteCachorro.java

---
+++ SolucaoAula07/TesteCachorro.java
@@ -0,0 +1,18 @@
+//Solução Aula 07 - Exercício 1
+
+/*public class Cachorro extends Animal{
+public void latir(){
+System.out,println("Au au");
+}
+}
+*/
+
+
+//Classe teste abaixo
+
+public class TesteCachorro {
+    public static void main(String[] args) {
+        Cachorro cachorro = new Cachorro();
+        cachorro.latir();
+    }
+}
