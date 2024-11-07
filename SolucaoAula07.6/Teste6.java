a7443f891d7e691074e7054c05c072dd0265f002
Miriam <miriamgama456@gmail.com>
2024-11-07 15:12:23 -0300

    Create Teste6.java

---
+++ SolucaoAula07.6/Teste6.java
@@ -0,0 +1,11 @@
+//Aula 07 exercício 6
+public class Teste6 {
+    public static void main(String[] args) {
+        Animal animal = new Animal();
+        animal.mover();
+
+        Cachorro cachorro = new Cachorro();
+        cachorro.mover();
+        cachorro.latir();
+    }
+}
