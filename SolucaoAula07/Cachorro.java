ac12eb30ed9cd694065cb21cbe6d841df674c0ce
Miriam <miriamgama456@gmail.com>
2024-11-07 12:30:12 -0300

    Delete SolucaoAula07 directory

--- SolucaoAula07/Cachorro.java
+++
@@ -1,9 +0,0 @@
-//Solução 7 - Exercício 1
-
-public class Cachorro extends Animal{
-
-    public void latir(){
-        System.out.println("au au");
-    }
-
-}
--- SolucaoAula07/Teste1.java
+++
@@ -1,9 +0,0 @@
-//Solução 7 - Exercício 1
-
-public class Teste1 {
-    public static void main(String[] args) {
-      Cachorro cachorro = new Cachorro();
-      cachorro.mover();
-      cachorro.latir();
-    }
-}
