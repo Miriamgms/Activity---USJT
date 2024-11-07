069b4542cf44b9f9a97aab1b4bcdce96917b946a
Miriam <miriamgama456@gmail.com>
2024-11-07 09:26:22 -0300

    Create Teste_Retangulo.java

---
+++ SolucaoAula07/Teste_Retangulo.java
@@ -0,0 +1,32 @@
+//Solução Aula 07 - Exercício 2
+
+/*public abstract class Forma
+{
+public abstract double calcularArea();
+}
+-------------------------------------
+public class Retangulo extends Forma
+{
+private double largura;
+private double altura;
+
+public Retangulo(double largura, double altura){
+this.largura = largura;
+this.altura = altura;
+}
+public double calcularPerimetro()
+{
+return 2 * (largura + altura);
+}
+}
+*/
+
+//classe teste abaixo
+
+public class Teste_Retangulo 
+{
+    public static void main(String[] args) {
+        Retangulo retangulo = new Retangulo(5, 3);
+        System.out.println("Perímetro calculado: " + retangulo.calcularPerimetro());
+    }
+}
