e73c66b967691fa674b6ffe81d67cadde575b4ad
Miriam <miriamgama456@gmail.com>
2024-11-07 09:44:53 -0300

    Update Teste_Retangulo.java

--- SolucaoAula07/Teste_Retangulo.java
+++ SolucaoAula07/Teste_Retangulo.java
@@ -25,11 +25,24 @@ return 2 * (largura + altura);
 
 //classe teste abaixo
 
+import javax.swing.JOptionPane;
+
 public class Teste_Retangulo {
     public static void main(String[] args) {
-        Retangulo retangulo = new Retangulo(5, 3);
-        System.out.println("Área: " + retangulo.calcularArea());
-        System.out.println("Perímetro: " + retangulo.calcularPerimetro());
+        String sLargura = JOptionPane.showInputDialog("Digite a largura do retângulo:");
+        String sAltura = JOptionPane.showInputDialog("Digite a altura do retângulo:");
+        
+        double largura = Double.parseDouble(sLargura);
+        double altura = Double.parseDouble(sAltura);
+        
+        Retangulo retangulo = new Retangulo(largura, altura);
+        
+        double area = retangulo.calcularArea();
+        double perimetro = retangulo.calcularPerimetro();
+        
+        String resultado = "Área: " + area + "\nPerímetro: " + perimetro;
+        JOptionPane.showMessageDialog(null, resultado, "Resultado do Retângulo", JOptionPane.INFORMATION_MESSAGE);
     }
 }
+
 
