1e079a9011030cc54f3c5220349b35af3a5a9013
Miriam <miriamgama456@gmail.com>
2024-11-07 09:41:15 -0300

    Update Teste_Retangulo.java

--- SolucaoAula07/Teste_Retangulo.java
+++ SolucaoAula07/Teste_Retangulo.java
@@ -14,19 +14,22 @@ public Retangulo(double largura, double altura){
 this.largura = largura;
 this.altura = altura;
 }
+public double calcularArea(){
+return largura * altura;
+}
 public double calcularPerimetro()
 {
 return 2 * (largura + altura);
 }
-}
-*/
+} */
 
 //classe teste abaixo
 
-public class Teste_Retangulo 
-{
+public class TesteRetangulo {
     public static void main(String[] args) {
         Retangulo retangulo = new Retangulo(5, 3);
-        System.out.println("Perímetro calculado: " + retangulo.calcularPerimetro());
+        System.out.println("Área: " + retangulo.calcularArea());
+        System.out.println("Perímetro: " + retangulo.calcularPerimetro());
     }
 }
+
