24daf762af36b018cdcd9387fdede1395150d59e
Miriam <miriamgama456@gmail.com>
2024-11-07 15:02:04 -0300

    Create Cavalo.java

---
+++ SolucaoAula07.7/Cavalo.java
@@ -0,0 +1,10 @@
+// Aula 07 Exercício 7
+
+
+public class Cavalo extends Animal
+{
+    public void mover() {
+        super.mover();
+        System.out.println("O cavalo está galopando");
+    }
+}
