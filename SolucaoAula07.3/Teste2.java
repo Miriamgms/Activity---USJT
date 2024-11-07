770efc1c3c18413b3ceb9b8835a38c09eccc74b9
Miriam <miriamgama456@gmail.com>
2024-11-07 12:54:37 -0300

    Create Teste2.java

---
+++ SolucaoAula07.3/Teste2.java
@@ -0,0 +1,13 @@
+//Aula 07 exercício 3
+
+import javax.swing.*;
+
+public class Teste2 {
+
+    public static void main(String[] args) {
+        Cachorro c = new Cachorro();
+        c.fazerSom();
+        c.mover();
+    }
+
+}
