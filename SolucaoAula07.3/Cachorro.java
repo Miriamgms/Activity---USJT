a6163dbc7567332406fa100742e17f2b4557b07c
Miriam <miriamgama456@gmail.com>
2024-11-07 12:53:54 -0300

    Create Cachorro.java

---
+++ SolucaoAula07.3/Cachorro.java
@@ -0,0 +1,12 @@
+//Aula 07 exercício 3
+
+public class Cachorro implements Animal{
+    public void fazerSom(){
+        System.out.println("au au");
+    }
+
+
+    public void mover() {
+        System.out.println("O cachorro está correndo");
+    }
+}
